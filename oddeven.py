4acfb2a81be2e051c15faddd7b16e5b8f01fb462
TjHolm13 <tatem.holm@mnsu.edu>
2024-09-20 10:39:41 -0500

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,13 @@
+"""
+Tatem Holm
+9/17/2024
+This program will tell you if a number is odd or even
+"""
+
+user_num = int(input('Please enter a number: '))
+
+if user_num % 2 == 1:
+	print(f'{user_num} is odd')
+else:
+	print(f'{user_num} is even')
+		
